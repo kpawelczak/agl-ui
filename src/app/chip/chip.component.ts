fa54b23498dd641410008e7651ce448fd7cc2d82
drhu <lbkrpaw87@gmail.com>
2019-02-21 11:22:54 +0100

    new chip.scss

--- src/app/chip/chip.component.ts
+++ src/app/chip/chip.component.ts
@@ -3,6 +3,6 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'gui-chip',
   templateUrl: './chip.component.html',
-  styleUrls: ['./chip.component.scss'],
+  styleUrls: ['./chip.scss'],
   })
 export class ChipComponent{}
