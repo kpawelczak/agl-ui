bce6fee08448bc8ffd171c85975c2e7a710fd566
drhu <lbkrpaw87@gmail.com>
2019-02-21 11:37:04 +0100

    Update fabric-chip.module.ts

--- src/app/chip/fabric-chip.module.ts
+++ src/app/chip/fabric-chip.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChipComponent } from './chip.component';
-import './chip.scss';
+
 @NgModule({
   declarations: [
     
